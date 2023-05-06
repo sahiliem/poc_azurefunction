a8f944d73faa4792cb570963d615c3a41c5815c0
sahiliem <mail.analsarkar@gmail.com>
2023-05-06 18:09:27 +0530

    Update my_server.py

--- apps/my_server.py
+++ apps/my_server.py
@@ -1,40 +1,21 @@
-import json
-import os
-
-import uvicorn
-from fastapi import FastAPI
-
-from fastapi.middleware.cors import CORSMiddleware
-
-app = FastAPI()
-
-origins = ["*"]
-
-# origins = [
-#     "http://localhost.tiangolo.com",
-#     "https://localhost.tiangolo.com",
-#     "http://localhost",
-#     "http://localhost:8080",
-# ]
+import logging
+import azure.functions as func
+from FastAPIApp import app  # Main API application
 
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+@app.get("/sample")
+async def index():
+    return {
+        "info": "Try /hello/Shivani for parameterized route.",
+    }
 
 
-@app.get("/test")
-def read_root():
-    return {"Hello": "World!!!"}
+@app.get("/hello/{name}")
+async def get_name(name: str):
+    return {
+        "name": name,
+    }
 
 
-if __name__ == '__main__':
-    host_name = os.getenv('SERVER_HOST', '127.0.0.1')
-    print(host_name)
-    print("TESTING")
-    uvicorn.run(app, host='0.0.0.0', port=8010)
-    print("running")
+async def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    return await func.AsgiMiddleware(app).handle_async(req, context)
